8070cd88f2f8b2fb68e49c34c878ac78d14a9627
Jackson Bates <jkbates222@gmail.com>
2018-09-27 19:57:24 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-  $("p");
-});
+function paragraphSelector() {
+  $(document).ready(function(){
+    $("p");
+  });
+}
 
 function lastImageSelector() {
   $('div:last');
